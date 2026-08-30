3c136bc33353ff468604acb879bba32310fad683
gauravpatel.14 <gp081947@gmail.com>
2026-08-30 15:34:31 +0530

    new

---
+++ assignment3/assignment2.js
@@ -0,0 +1,4 @@
+console.log("Name: Gaurav");
+console.log("Education: B.Tech in Computer Science and Engineering");
+console.log("Current Learning: Node.js");
+console.log("Goal: Full Stack Developer");
